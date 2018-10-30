500d2c9dbed73c447cbcaafe0ea5929ff0dceadb
ish-vlad <vladislav.ishimtsev@skoltech.ru>
2018-10-31 00:15:15 +0300

    Generate KD features for MNIST (unverified)

--- workspace/generate_features_MNIST.py
+++ workspace/generate_features_MNIST.py
@@ -1,6 +1,108 @@
 # input: X_train (N, 256)
-
+#
 # for each point cloud generate KD features from different levels
 # and finally for the whole point  cloud
-
+#
 # output: several X_train_128x(3+32).py --- 256 points map to 128 points with 3 dimension (X,Y,color) and 32 features
+
+import tqdm
+import theano
+import h5py as h5
+import numpy as np
+
+from lasagne.layers import get_output, get_all_params
+
+from lib.KDNet import KDNetwork, iterate_minibatches
+
+
+with h5.File('../datasets/MNIST_2D/mnist2d.h5', 'r') as hf:
+    X_train = np.array(hf.get('X_train'))
+    y_train = np.array(hf.get('y_train'))
+    X_test = np.array(hf.get('X_test'))
+    y_test = np.array(hf.get('y_test'))
+
+config = {
+    # General
+    'mode': 'test',
+    'batchsize': 2000,
+    'shuffle': False,
+    # Augmentations
+    'flip': False,
+    'ascale': True, 'as_min': 0.6667, 'as_max': 1.5,
+    'rotate': False, 'r_positions': 12, 'test_pos': None,
+    'translate': False, 't_rate': 0.1,
+    # Point clouds and kd-trees generation
+    'steps': 8, # also control the depth of the network
+    'dim': 3,
+    'lim': 1,
+    'det': False,
+    'gamma': 10.,
+    # NN options
+    'input_features': 'all', # 'all' for point coordinates, 'no' for feeding 1's as point features
+    'n_f': [16,
+            32,  32,
+            64,  128,
+            128, 256,
+            512, 128], # representation sizes
+    'n_output': 10,
+    'l2': 1e-3,
+    'lr': 1e-3,
+    'n_ens': 16 # number of predicts (take average)
+}
+
+model = KDNetwork(config)
+clouds, norms = KDNetwork.clouds, KDNetwork.norms
+KDNet = model.net
+
+
+def generate(X, y, X_name):
+    all_factors = []
+    # for each factor
+    for factor in range(1,8):
+        features_det = get_output(KDNet['cloud{}'.format(factor+1)], deterministic=True)
+        features_fun = theano.function([clouds] + norms, features_det, on_unused_input='ignore')
+
+        # for each batch of pointclouds
+        results = []
+        for i, batch in tqdm.tqdm(enumerate(iterate_minibatches(X, y, **config))):
+            # divide by sub-pointclouds
+            sub_pcs = batch[0].reshape(batch[0].shape[:2] + (-1, 2**factor))
+
+            # take center of mass
+            sub_pcs = sub_pcs.mean(axis=-1)
+
+            # take features from KD
+            features = features_fun(*(batch[:-1]))
+
+            results.append(np.concatenate((sub_pcs, features), axis=1).transpose(0,2,1))
+
+        all_factors.append(np.concatenate(results, axis=0))
+        print('factor', factor, 'Shape', all_factors[-1].shape)
+
+    root = []
+
+    features_det = get_output(KDNet['cloud_fin_bn'], deterministic=True)
+    features_fun = theano.function([clouds] + norms, features_det, on_unused_input='ignore')
+
+    for i, batch in enumerate(iterate_minibatches(X, y, **config)):
+        # take features from KD
+        features = features_fun(*(batch[:-1]))
+        root.append(features)
+
+    root = np.concatenate(root, axis=0).reshape(len(X_test), -1)
+
+    path = '../datasets/MNIST_2D/kd_features/' + X_name + '/'
+
+    np.save(path + 'X_' + X_name + '_256x3.npy', X_test)
+    np.save(path + 'X_' + X_name + '_128x(3+32).npy', all_factors[0])
+    np.save(path + 'X_' + X_name + '_064x(3+32).npy', all_factors[1])
+    np.save(path + 'X_' + X_name + '_032x(3+64).npy', all_factors[2])
+    np.save(path + 'X_' + X_name + '_016x(3+128).npy', all_factors[3])
+    np.save(path + 'X_' + X_name + '_008x(3+128).npy', all_factors[4])
+    np.save(path + 'X_' + X_name + '_004x(3+256).npy', all_factors[5])
+    np.save(path + 'X_' + X_name + '_002x(3+512).npy', all_factors[6])
+    np.save(path + 'X_' + X_name + '_001x128(root).npy', root)
+
+
+generate(X_train, y_train, 'train')
+generate(X_test, y_test, 'test')
---
+++ workspace/lib/KDNet.py
@@ -0,0 +1,222 @@
+import numpy as np
+import theano.gpuarray
+theano.gpuarray.use('cuda1')
+import theano.tensor as T
+
+from lasagne.layers import InputLayer
+from lasagne.layers import ReshapeLayer
+from lasagne.layers import NonlinearityLayer
+from lasagne.layers import ExpressionLayer
+from lasagne.layers import ElemwiseSumLayer, ElemwiseMergeLayer
+from lasagne.layers import DenseLayer
+from lasagne.layers import BatchNormLayer
+from lasagne.nonlinearities import rectify, softmax
+
+from KDNetwork.lib.nn.layers import SharedDotLayer, SPTNormReshapeLayer
+from KDNetwork.lib.nn.utils import load_weights
+from KDNetwork.lib.trees.kdtrees import KDTrees
+
+
+class KDNetwork:
+    def __init__(self, config):
+        self.clouds = T.tensor3(dtype='float64')
+        self.norms = [T.tensor3(dtype='float64') for _ in xrange(config['steps'])]
+        self.target = T.vector(dtype='int64')
+
+        self.inputs = [self.clouds, self.norms, self.target]
+
+        KDNet = {}
+        if config['input_features'] == 'no':
+            KDNet['input'] = InputLayer((None, 1, 2 ** config['steps']), input_var=self.clouds)
+        else:
+            KDNet['input'] = InputLayer((None, 3, 2 ** config['steps']), input_var=self.clouds)
+        for i in xrange(config['steps']):
+            KDNet['norm{}_r'.format(i + 1)] = InputLayer((None, 3, 2 ** (config['steps'] - 1 - i)), input_var=self.norms[i])
+            KDNet['norm{}_l'.format(i + 1)] = ExpressionLayer(KDNet['norm{}_r'.format(i + 1)], lambda X: -X)
+
+            KDNet['norm{}_l_X-'.format(i + 1)] = SPTNormReshapeLayer(KDNet['norm{}_l'.format(i + 1)], '-', 0,
+                                                                     config['n_f'][i + 1])
+            KDNet['norm{}_l_Y-'.format(i + 1)] = SPTNormReshapeLayer(KDNet['norm{}_l'.format(i + 1)], '-', 1,
+                                                                     config['n_f'][i + 1])
+            KDNet['norm{}_l_Z-'.format(i + 1)] = SPTNormReshapeLayer(KDNet['norm{}_l'.format(i + 1)], '-', 2,
+                                                                     config['n_f'][i + 1])
+            KDNet['norm{}_l_X+'.format(i + 1)] = SPTNormReshapeLayer(KDNet['norm{}_l'.format(i + 1)], '+', 0,
+                                                                     config['n_f'][i + 1])
+            KDNet['norm{}_l_Y+'.format(i + 1)] = SPTNormReshapeLayer(KDNet['norm{}_l'.format(i + 1)], '+', 1,
+                                                                     config['n_f'][i + 1])
+            KDNet['norm{}_l_Z+'.format(i + 1)] = SPTNormReshapeLayer(KDNet['norm{}_l'.format(i + 1)], '+', 2,
+                                                                     config['n_f'][i + 1])
+            KDNet['norm{}_r_X-'.format(i + 1)] = SPTNormReshapeLayer(KDNet['norm{}_r'.format(i + 1)], '-', 0,
+                                                                     config['n_f'][i + 1])
+            KDNet['norm{}_r_Y-'.format(i + 1)] = SPTNormReshapeLayer(KDNet['norm{}_r'.format(i + 1)], '-', 1,
+                                                                     config['n_f'][i + 1])
+            KDNet['norm{}_r_Z-'.format(i + 1)] = SPTNormReshapeLayer(KDNet['norm{}_r'.format(i + 1)], '-', 2,
+                                                                     config['n_f'][i + 1])
+            KDNet['norm{}_r_X+'.format(i + 1)] = SPTNormReshapeLayer(KDNet['norm{}_r'.format(i + 1)], '+', 0,
+                                                                     config['n_f'][i + 1])
+            KDNet['norm{}_r_Y+'.format(i + 1)] = SPTNormReshapeLayer(KDNet['norm{}_r'.format(i + 1)], '+', 1,
+                                                                     config['n_f'][i + 1])
+            KDNet['norm{}_r_Z+'.format(i + 1)] = SPTNormReshapeLayer(KDNet['norm{}_r'.format(i + 1)], '+', 2,
+                                                                     config['n_f'][i + 1])
+
+            KDNet['cloud{}'.format(i + 1)] = SharedDotLayer(KDNet['input'], config['n_f'][i]) if i == 0 else \
+                ElemwiseSumLayer([KDNet['cloud{}_l_X-_masked'.format(i)],
+                                  KDNet['cloud{}_l_Y-_masked'.format(i)],
+                                  KDNet['cloud{}_l_Z-_masked'.format(i)],
+                                  KDNet['cloud{}_l_X+_masked'.format(i)],
+                                  KDNet['cloud{}_l_Y+_masked'.format(i)],
+                                  KDNet['cloud{}_l_Z+_masked'.format(i)],
+                                  KDNet['cloud{}_r_X-_masked'.format(i)],
+                                  KDNet['cloud{}_r_Y-_masked'.format(i)],
+                                  KDNet['cloud{}_r_Z-_masked'.format(i)],
+                                  KDNet['cloud{}_r_X+_masked'.format(i)],
+                                  KDNet['cloud{}_r_Y+_masked'.format(i)],
+                                  KDNet['cloud{}_r_Z+_masked'.format(i)]])
+            KDNet['cloud{}_bn'.format(i + 1)] = BatchNormLayer(KDNet['cloud{}'.format(i + 1)])
+            KDNet['cloud{}_relu'.format(i + 1)] = NonlinearityLayer(KDNet['cloud{}_bn'.format(i + 1)], rectify)
+
+            KDNet['cloud{}_r'.format(i + 1)] = ExpressionLayer(KDNet['cloud{}_relu'.format(i + 1)],
+                                                               lambda X: X[:, :, 1::2],
+                                                               (None, config['n_f'][i], 2 ** (config['steps'] - i - 1)))
+            KDNet['cloud{}_l'.format(i + 1)] = ExpressionLayer(KDNet['cloud{}_relu'.format(i + 1)],
+                                                               lambda X: X[:, :, ::2],
+                                                               (None, config['n_f'][i], 2 ** (config['steps'] - i - 1)))
+
+            KDNet['cloud{}_l_X-'.format(i + 1)] = SharedDotLayer(KDNet['cloud{}_l'.format(i + 1)], config['n_f'][i + 1])
+            KDNet['cloud{}_l_Y-'.format(i + 1)] = SharedDotLayer(KDNet['cloud{}_l'.format(i + 1)], config['n_f'][i + 1])
+            KDNet['cloud{}_l_Z-'.format(i + 1)] = SharedDotLayer(KDNet['cloud{}_l'.format(i + 1)], config['n_f'][i + 1])
+            KDNet['cloud{}_l_X+'.format(i + 1)] = SharedDotLayer(KDNet['cloud{}_l'.format(i + 1)], config['n_f'][i + 1])
+            KDNet['cloud{}_l_Y+'.format(i + 1)] = SharedDotLayer(KDNet['cloud{}_l'.format(i + 1)], config['n_f'][i + 1])
+            KDNet['cloud{}_l_Z+'.format(i + 1)] = SharedDotLayer(KDNet['cloud{}_l'.format(i + 1)], config['n_f'][i + 1])
+            KDNet['cloud{}_r_X-'.format(i + 1)] = SharedDotLayer(KDNet['cloud{}_r'.format(i + 1)], config['n_f'][i + 1],
+                                                                 W=KDNet['cloud{}_l_X-'.format(i + 1)].W,
+                                                                 b=KDNet['cloud{}_l_X-'.format(i + 1)].b)
+            KDNet['cloud{}_r_Y-'.format(i + 1)] = SharedDotLayer(KDNet['cloud{}_r'.format(i + 1)], config['n_f'][i + 1],
+                                                                 W=KDNet['cloud{}_l_Y-'.format(i + 1)].W,
+                                                                 b=KDNet['cloud{}_l_Y-'.format(i + 1)].b)
+            KDNet['cloud{}_r_Z-'.format(i + 1)] = SharedDotLayer(KDNet['cloud{}_r'.format(i + 1)], config['n_f'][i + 1],
+                                                                 W=KDNet['cloud{}_l_Z-'.format(i + 1)].W,
+                                                                 b=KDNet['cloud{}_l_Z-'.format(i + 1)].b)
+            KDNet['cloud{}_r_X+'.format(i + 1)] = SharedDotLayer(KDNet['cloud{}_r'.format(i + 1)], config['n_f'][i + 1],
+                                                                 W=KDNet['cloud{}_l_X+'.format(i + 1)].W,
+                                                                 b=KDNet['cloud{}_l_X+'.format(i + 1)].b)
+            KDNet['cloud{}_r_Y+'.format(i + 1)] = SharedDotLayer(KDNet['cloud{}_r'.format(i + 1)], config['n_f'][i + 1],
+                                                                 W=KDNet['cloud{}_l_Y+'.format(i + 1)].W,
+                                                                 b=KDNet['cloud{}_l_Y+'.format(i + 1)].b)
+            KDNet['cloud{}_r_Z+'.format(i + 1)] = SharedDotLayer(KDNet['cloud{}_r'.format(i + 1)], config['n_f'][i + 1],
+                                                                 W=KDNet['cloud{}_l_Z+'.format(i + 1)].W,
+                                                                 b=KDNet['cloud{}_l_Z+'.format(i + 1)].b)
+
+            KDNet['cloud{}_l_X-_masked'.format(i + 1)] = ElemwiseMergeLayer([KDNet['cloud{}_l_X-'.format(i + 1)],
+                                                                             KDNet['norm{}_l_X-'.format(i + 1)]], T.mul)
+            KDNet['cloud{}_l_Y-_masked'.format(i + 1)] = ElemwiseMergeLayer([KDNet['cloud{}_l_Y-'.format(i + 1)],
+                                                                             KDNet['norm{}_l_Y-'.format(i + 1)]], T.mul)
+            KDNet['cloud{}_l_Z-_masked'.format(i + 1)] = ElemwiseMergeLayer([KDNet['cloud{}_l_Z-'.format(i + 1)],
+                                                                             KDNet['norm{}_l_Z-'.format(i + 1)]], T.mul)
+            KDNet['cloud{}_l_X+_masked'.format(i + 1)] = ElemwiseMergeLayer([KDNet['cloud{}_l_X+'.format(i + 1)],
+                                                                             KDNet['norm{}_l_X+'.format(i + 1)]], T.mul)
+            KDNet['cloud{}_l_Y+_masked'.format(i + 1)] = ElemwiseMergeLayer([KDNet['cloud{}_l_Y+'.format(i + 1)],
+                                                                             KDNet['norm{}_l_Y+'.format(i + 1)]], T.mul)
+            KDNet['cloud{}_l_Z+_masked'.format(i + 1)] = ElemwiseMergeLayer([KDNet['cloud{}_l_Z+'.format(i + 1)],
+                                                                             KDNet['norm{}_l_Z+'.format(i + 1)]], T.mul)
+            KDNet['cloud{}_r_X-_masked'.format(i + 1)] = ElemwiseMergeLayer([KDNet['cloud{}_r_X-'.format(i + 1)],
+                                                                             KDNet['norm{}_r_X-'.format(i + 1)]], T.mul)
+            KDNet['cloud{}_r_Y-_masked'.format(i + 1)] = ElemwiseMergeLayer([KDNet['cloud{}_r_Y-'.format(i + 1)],
+                                                                             KDNet['norm{}_r_Y-'.format(i + 1)]], T.mul)
+            KDNet['cloud{}_r_Z-_masked'.format(i + 1)] = ElemwiseMergeLayer([KDNet['cloud{}_r_Z-'.format(i + 1)],
+                                                                             KDNet['norm{}_r_Z-'.format(i + 1)]], T.mul)
+            KDNet['cloud{}_r_X+_masked'.format(i + 1)] = ElemwiseMergeLayer([KDNet['cloud{}_r_X+'.format(i + 1)],
+                                                                             KDNet['norm{}_r_X+'.format(i + 1)]], T.mul)
+            KDNet['cloud{}_r_Y+_masked'.format(i + 1)] = ElemwiseMergeLayer([KDNet['cloud{}_r_Y+'.format(i + 1)],
+                                                                             KDNet['norm{}_r_Y+'.format(i + 1)]], T.mul)
+            KDNet['cloud{}_r_Z+_masked'.format(i + 1)] = ElemwiseMergeLayer([KDNet['cloud{}_r_Z+'.format(i + 1)],
+                                                                             KDNet['norm{}_r_Z+'.format(i + 1)]], T.mul)
+
+        KDNet['cloud_fin'] = ElemwiseSumLayer([KDNet['cloud{}_l_X-_masked'.format(config['steps'])],
+                                               KDNet['cloud{}_l_Y-_masked'.format(config['steps'])],
+                                               KDNet['cloud{}_l_Z-_masked'.format(config['steps'])],
+                                               KDNet['cloud{}_l_X+_masked'.format(config['steps'])],
+                                               KDNet['cloud{}_l_Y+_masked'.format(config['steps'])],
+                                               KDNet['cloud{}_l_Z+_masked'.format(config['steps'])],
+                                               KDNet['cloud{}_r_X-_masked'.format(config['steps'])],
+                                               KDNet['cloud{}_r_Y-_masked'.format(config['steps'])],
+                                               KDNet['cloud{}_r_Z-_masked'.format(config['steps'])],
+                                               KDNet['cloud{}_r_X+_masked'.format(config['steps'])],
+                                               KDNet['cloud{}_r_Y+_masked'.format(config['steps'])],
+                                               KDNet['cloud{}_r_Z+_masked'.format(config['steps'])]])
+        KDNet['cloud_fin_bn'] = BatchNormLayer(KDNet['cloud_fin'])
+        KDNet['cloud_fin_relu'] = NonlinearityLayer(KDNet['cloud_fin_bn'], rectify)
+        KDNet['cloud_fin_reshape'] = ReshapeLayer(KDNet['cloud_fin_relu'], (-1, config['n_f'][-1]))
+        KDNet['output'] = DenseLayer(KDNet['cloud_fin_reshape'], config['n_output'], nonlinearity=softmax)
+
+        # Loading weights (optional)
+        load_weights('./KDNetwork/models/RT_AS+TR_2D_MNIST.pkl', KDNet['output'])
+
+        self.net = KDNet
+
+
+def iterate_minibatches(*arrays, **kwargs):
+    if kwargs['mode'] == 'train':
+        indices = np.random.choice(len(arrays[0]), size = len(arrays[0]) - len(arrays[0]) % kwargs['batchsize'])
+    elif kwargs['mode'] == 'test':
+        indices = np.arange(len(arrays[0]))
+
+    if kwargs['shuffle']:
+        np.random.shuffle(indices)
+
+    for start_idx in xrange(0, len(indices), kwargs['batchsize']):
+        excerpt = indices[start_idx:start_idx + kwargs['batchsize']]
+        tmp = np.transpose(arrays[0][excerpt], axes=(0,2,1))
+
+        # random flipping along x and y axes
+        if kwargs['flip']:
+            flip = np.random.random(size=(len(tmp), 2, 1))
+            flip[flip >= 0.5] = 1.
+            flip[flip < 0.5] = -1.
+            tmp[:, :2] *= flip
+
+        # stretch picture
+        if kwargs['ascale']:
+            tmp *= (kwargs['as_min'] + (kwargs['as_max'] - kwargs['as_min'])*np.random.random(size=(len(tmp), kwargs['dim'], 1)))
+            tmp = tmp / np.fabs(tmp).max(axis=(1, 2), keepdims=True)
+
+        if kwargs['rotate']:
+            r = np.sqrt((tmp[:, :2]**2).sum(axis=1))
+            coss = tmp[:, 0]/r
+            sins = tmp[:, 1]/r
+
+            if kwargs['test_pos'] is not None:
+                alpha = 2*np.pi*kwargs['test_pos']/kwargs['r_positions']
+            else:
+                alpha = 2*np.pi*np.random.randint(0, kwargs['r_positions'], (len(tmp), 1))/kwargs['positions']
+
+            cosr = np.cos(alpha)
+            sinr = np.sin(alpha)
+            cos = coss*cosr - sins*sinr
+            sin = sins*cosr + sinr*coss
+            tmp[:, 0] = r*cos
+            tmp[:, 1] = r*sin
+
+        # translate along all axes
+        if kwargs['translate']:
+            mins = tmp.min(axis=2, keepdims=True)
+            maxs = tmp.max(axis=2, keepdims=True)
+            rngs = maxs - mins
+            tmp += kwargs['t_rate']*(np.random.random(size=(len(tmp), kwargs['dim'], 1)) - 0.5)*rngs
+
+        trees_data = KDTrees(tmp[:,:2], dim=kwargs['dim']-1, steps=kwargs['steps'],
+                                 lim=kwargs['lim'], det=kwargs['det'], gamma=kwargs['gamma'], medians=True)
+
+        sortings, normals = trees_data['sortings'], trees_data['normals']
+
+        if kwargs['input_features'] == 'all':
+            clouds = np.empty((len(excerpt), kwargs['dim'], 2**kwargs['steps']), dtype=np.float32)
+            for i, srt in enumerate(sortings):
+                clouds[i] = tmp[i, :, srt].T
+        elif kwargs['input_features'] == 'no':
+            clouds = np.ones((len(excerpt), 1, 2**kwargs['steps']), dtype=np.float32)
+
+        if kwargs['mode'] == 'train':
+            yield [clouds] + normals[::-1] + [arrays[1][excerpt]]
+        if kwargs['mode'] == 'test':
+            yield [clouds] + normals[::-1] + [excerpt]
