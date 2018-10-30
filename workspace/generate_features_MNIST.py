c58f151627b98a8385ef2fb74f61fe575b241902
ish-vlad <vladislav.ishimtsev@skoltech.ru>
2018-10-30 23:29:37 +0300

    Make templates for further plan

---
+++ workspace/generate_features_MNIST.py
@@ -0,0 +1,6 @@
+# input: X_train (N, 256)
+
+# for each point cloud generate KD features from different levels
+# and finally for the whole point  cloud
+
+# output: several X_train_128x(3+32).py --- 256 points map to 128 points with 3 dimension (X,Y,color) and 32 features
