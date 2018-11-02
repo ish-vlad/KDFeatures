e61dbedf973af121ab5fc11d989b8b9e43b00d20
ish-vlad <vladislav.ishimtsev@skoltech.ru>
2018-11-02 22:59:37 +0300

    Store data

---
+++ workspace/store.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+cd ../store
+mkdir buf/ buf/results buf/datasets
+
+mv ../datasets/MNIST_2D/Rotation buf/datasets/
+mv ../datasets/MNIST_2D/Translation buf/datasets/
+mv ../datasets/MNIST_2D/Rotation_and_Translation buf/datasets/
+
+cp -r ../results/MNIST_2D/* buf/results/
+
+zip -r v1.zip buf
+
+cd ../workspace
